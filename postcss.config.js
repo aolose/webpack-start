0a7c7860b451ab40e8475dfe2b6e0b8716a34747
tom.z <tom.z@tc-gaming.com>
2018-11-26 19:04:52 +0800

    config post css

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,13 @@ module.exports = {
             stage: 4,
             browsers: 'last 2 versions',
         },
+        'css-mqpacker':{ sort: true },
+        'postcss-svgo':{},
+        'postcss-normalize':{
+            browsers: 'last 2 versions'
+        },
         'cssnano': {
+            'mergeRules':true,
             'preset': [
                 'default',
                 {'discardComments': {'removeAll': true}}
