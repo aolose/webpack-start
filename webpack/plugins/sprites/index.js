304bb1186e24a7bf6c48d04d865481fdd806496b
tom.z <tom.z@tc-gaming.com>
2018-11-26 15:36:10 +0800

    sprite plugin: support watch mode

--- webpack/plugins/sprites/index.js
+++ webpack/plugins/sprites/index.js
@@ -1,6 +1,7 @@
 const ss = require('spritesmith');
 const tmp = require('./template');
 const path = require('path');
+
 function run(images, k, cb) {
     const ck = '$x' + (Math.random() * Date.now() * 1e4).toString(32).substr(0, 6);
     const o = {
@@ -28,7 +29,8 @@ function run(images, k, cb) {
 }
 
 const glob = require("glob");
-const fs =  require("fs");
+const fs = require("fs");
+
 function combineCss(ls) {
     let css = '';
     let v = '';
@@ -36,9 +38,9 @@ function combineCss(ls) {
     ls.forEach(f => {
         const {c: {k, d}, s: {o}} = f;
         v += `${k} : "${o}";\n`;
-        d.forEach(([n,x])=>{
+        d.forEach(([n, x]) => {
             let nm = n;
-            if (m[n]) nm += '_'+m[n]++;
+            if (m[n]) nm += '_' + m[n]++;
             else m[n] = 1;
             css += `$${nm} : ${x}`;
         });
@@ -48,35 +50,88 @@ function combineCss(ls) {
 
 module.exports = class Sprites {
     constructor(options) {
-        const {source = {}, scssPath = '_sprite.scss', base = './src/styles'} = options;
-        const ls = [];
-        const basePath = path.resolve(process.cwd(),base);
-        Object.keys(source).forEach(k => {
-            const p = source[k];
-            ls.push({out: k + '.png', files: glob.sync(path.resolve(basePath,p))})
-        });
-        this.files = ls || [];
-        this.scssPath = scssPath;
-        this.basePath = basePath;
+        this.options = options
     }
 
     apply(compiler) {
-        const {files, scssPath, basePath} = this;
-        const func = () => {
+        const {source = {}, scssPath = '_sprite.scss', base = './src/styles'} = this.options;
+        let inProcess = false;
+        let watched = false;
+        const initWatch = (d, f) => {
+            watched = true;
+            compiler.hooks.afterCompile.tap('after-compile',(cp)=>{
+                const dirs = Object.keys(d);
+                const files = Object.keys(f);
+                if (dirs.length) {
+                    dirs.forEach(o => cp.contextDependencies.add(o));
+                }
+                if (files.length) {
+                    files.forEach(o => cp.fileDependencies.add(o));
+                }
+            });
+        };
+        const func = isWatch => (cp, cb) => {
+            const done = () => {
+                inProcess = false;
+                cb && cb();
+            };
+            if (inProcess) return cb && cb();
+            const files = [];
+            const basePath = path.resolve(process.cwd(), base);
+            const dirs = {};
+            const ffs = {};
+            Object.keys(source).forEach(k => {
+                const p = source[k];
+                const ps = path.resolve(basePath, p);
+                const x = ps.indexOf('*');
+                const f = glob.sync(ps);
+                if (x) {
+                    dirs[ps.substr(0, x)] = 1;
+                } else if (/\*|[^.\\/]+[\\/]?$/.test(ps)) {
+                    dirs[ps] = 1
+                } else {
+                    f && f.forEach(v => ffs[v] = 1);
+                }
+                files.push({out: k + '.png', files: f})
+            });
+            inProcess = true;
             const l = files.length, rs = [];
+            if (!l) {
+                return done();
+            }
+            if (isWatch && !watched) initWatch(dirs, ffs);
+            if (isWatch && cp.watchMode) {
+                const wc = cp.watchFileSystem.watcher;
+                const mt = wc && wc.mtimes;
+                if (!mt || !Object.keys(mt).length) return cb && cb();
+                const ff = [];
+                files.forEach(o => {
+                    const f0 = o.files;
+                    for (let i = 0, l = f0.length; i < l; i++) {
+                        if (mt[f0[i].replace(/\//g,'\\')]) {
+                            return ff.push(o);
+                        }
+                    }
+                });
+                if (!ff.length) return done();
+                files.length = 0;
+                files.push(...ff);
+            }
             let i = 0;
             files.forEach(o => {
                 run(o.files, o.out, r => {
                     rs.push(r);
                     if (++i === l) {
-                        fs.writeFileSync(path.resolve(basePath,scssPath), combineCss(rs));
+                        fs.writeFileSync(path.resolve(basePath, scssPath), combineCss(rs));
                         rs.forEach(a => {
-                            fs.writeFileSync(path.resolve(basePath,a.s.o), a.s.d);
+                            fs.writeFileSync(path.resolve(basePath, a.s.o), a.s.d);
                         });
+                        done();
                     }
                 });
             });
         };
-        compiler.hooks.afterPlugins.tap('WebPackSprites', func);
+        compiler.hooks.afterPlugins.tap('WebPackSprites', func());
+        compiler.hooks.watchRun.tap('WebPackSprites', func(1));
     }
 };
